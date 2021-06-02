ac3ac71ae67d8955b2abe9c3b24cd377567a73eb
wdye <wdye@pindrop.com>
2021-06-02 02:01:06 -0400

    Resize podiums based on number of players

--- src/client/components/game/podium/Podium.js
+++ src/client/components/game/podium/Podium.js
@@ -1,9 +1,68 @@
 import React from 'react';
 import { Box, Flex } from '@chakra-ui/react';
-import { DEFAULT_FONT_STYLE } from '../../../../constants.mjs';
+import { DEFAULT_FONT_STYLE, MAX_PLAYER_NAME_LENGTH } from '../../../../constants.mjs';
+
+const FONT_SIZE_CLASSES = ['xs', 'sm', 'md', 'lg', 'xl'];
+const MIN_FONT_SIZE_INDEX = 0;
+const MAX_FONT_SIZE_INDEX = FONT_SIZE_CLASSES.length - 1;
+
+const LARGE_FONTS = [
+  'Beth Ellen',
+  'Homemade Apple',
+  'Rock Salt',
+];
+
+const SMALL_FONTS = [
+  'Rouge Script',
+  'Sacramento',
+];
+
+function isLargeFont(font) {
+  return (LARGE_FONTS.indexOf(font) !== -1);
+}
+
+function isSmallFont(font) {
+  return (SMALL_FONTS.indexOf(font) !== -1);
+}
+
+function getNameClasses(name, font, size) {
+  let index = MAX_FONT_SIZE_INDEX - 1;
+  if (size === 'xs') {
+    index = MIN_FONT_SIZE_INDEX;
+    if (!isLargeFont(font)) {
+      index += 1;
+    }
+    if (name.length < MAX_PLAYER_NAME_LENGTH - 3) {
+      index += 1;
+    }
+  } else {
+    if (size === 'sm') {
+      index -= 1;
+      if (name.length > MAX_PLAYER_NAME_LENGTH - 3) {
+        index -= 1;
+      }
+    } else {
+      if (name.length === MAX_PLAYER_NAME_LENGTH) {
+        index -= 2;
+      } else if (name.length === MAX_PLAYER_NAME_LENGTH - 1) {
+        index -= 1;
+      }
+    }
+    if (isLargeFont(font)) {
+      index -= 1;
+    }
+  }
+  if (isSmallFont(font)) {
+    index += 1;
+  }
+  index = Math.max(MIN_FONT_SIZE_INDEX, Math.min(index, MAX_FONT_SIZE_INDEX));
+  const sizeName = FONT_SIZE_CLASSES[index];
+  return `podium-name podium-name-${sizeName}`;
+}
 
 function Podium(props) {
-  let wrapperClasses = 'podium';
+  const size = props.size || 'lg';
+  let wrapperClasses = `podium podium-${size}`;
   if (props.onClick) {
     wrapperClasses += ' hover-pointer';
   }
@@ -15,18 +74,19 @@ function Podium(props) {
   } else {
     score = '$' + score;
   }
-  let nameClasses = 'podium-name';
+  let nameClasses = getNameClasses(props.name, props.preferredFontStyle, size);
   if (props.active) {
     nameClasses += ' podium-name-active';
   }
+  const mx = (size === 'xs' ? 6 : (size === 'sm' ? 8 : 12));
   const fontStyle = props.preferredFontStyle || DEFAULT_FONT_STYLE;
   return (
-    <Flex className={wrapperClasses} mb={2} mx={12} onClick={props.onClick} textAlign="center" userSelect="none">
+    <Flex className={wrapperClasses} mb={2} mx={mx} onClick={props.onClick} textAlign="center" userSelect="none">
       <Box className="podium-left-side podium-side">
         <Box className="podium-stripe" />
         <Box className="podium-stripe" />
       </Box>
-      <Box className="podium-center">
+      <Box className={`podium-center podium-center-${size}`}>
         <Box className={scoreClasses} py={2}>{score}</Box>
         <Box className={nameClasses} borderRadius="md" fontFamily={fontStyle} m={2} py={2}>{props.name}</Box>
       </Box>
--- src/client/components/game/podium/Podiums.js
+++ src/client/components/game/podium/Podiums.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { Flex } from '@chakra-ui/react';
 import Podium from './Podium';
 
+function getSize(players) {
+  const numPlayers = Object.keys(players).length;
+  if (numPlayers >= 6) {
+    return 'xs';
+  } else if (numPlayers === 5) {
+    return 'sm';
+  } else if (numPlayers === 4) {
+    return 'md';
+  }
+  return 'lg';
+}
+
 function Podiums(props) {
+  const size = getSize(props.players);
   const podiums = Object.values(props.players).map(player => {
     let onClick;
     if (player.playerID === props.playerID) {
@@ -10,7 +23,7 @@ function Podiums(props) {
     }
     const active = (player.playerID === props.playerAnswering);
     return <Podium key={player.playerID} name={player.name} score={player.score || 0} onClick={onClick}
-                   preferredFontStyle={player.preferredFontStyle} active={active} />
+                   preferredFontStyle={player.preferredFontStyle} active={active} size={size} />
   });
   return (
     <Flex className="podium-container" justify="center">
